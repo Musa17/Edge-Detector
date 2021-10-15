d3839fceabe7f6f2c3401c96733ea997ff1667e4
Musa17 <amusul00@fesb.hr>
2021-10-15 21:48:12 +0200

    Creating and printing filters

--- Edge Detector/Edge_Detector.py
+++ Edge Detector/Edge_Detector.py
@@ -1,4 +1,12 @@
 import cv2
 import numpy as np
 
+# Load image
+image = cv2.imread('Pikachu.jpg')
 
+# Creating and printing filters
+Hor_filter = np.array([[-1,0,1],[-2,0,2],[-1,0,1]])
+Vert_filter = np.array([[1,2,1],[0,0,0],[-1,-2,-1]])
+
+print(Hor_filter)
+print(Vert_filter)
