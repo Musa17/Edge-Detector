fbdca608ea08b216a41808d3eb8c3902472fc2c0
Musa17 <amusul00@fesb.hr>
2021-10-15 21:52:14 +0200

    Applying filters on image

--- Edge Detector/Edge_Detector.py
+++ Edge Detector/Edge_Detector.py
@@ -10,3 +10,12 @@ Vert_filter = np.array([[1,2,1],[0,0,0],[-1,-2,-1]])
 
 print(Hor_filter)
 print(Vert_filter)
+
+# Applying filters on image
+Gx = cv2.filter2D(image, -1, Hor_filter,
+anchor=(-1,-1), borderType=cv2.BORDER_CONSTANT)
+
+Gy = cv2.filter2D(image, -1, Vert_filter,
+anchor=(-1,-1), borderType=cv2.BORDER_CONSTANT)
+
+G = abs(Gx + Gy)
